7e13ff4025e9d039d95bf1c3f0322ad3e03c8ea6
Nicolás Londoño <nicolas.londono@veevart.com>
2023-05-21 22:52:46 -0500

    Add: encrypt password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 
 const createUser = async (req, res = express.request) => {
@@ -7,7 +8,16 @@ const createUser = async (req, res = express.request) => {
     newUser.birthdate = Date.parse(birthdate);
     newUser.type = 1;
     try {
-        const user = new User(newUser)
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El usuario ya existe"
+            })
+        }
+        user = new User(newUser)
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
         await user.save();
         res.status(200).json({
             ok: true,
