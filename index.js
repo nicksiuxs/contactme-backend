13e339c6a5a2d60678a4cd6e647069ada67547c0
Nicolás Londoño <nicolas.londono@veevart.com>
2023-05-21 20:52:12 -0500

    Add: db connection

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+const { dbConnection } = require("./database/config");
+
 const app = express();
+
+dbConnection();
 
 app.use(express.static("public"));
 
