c28f6f4e3dd6871280faf29d840aa437ffcfcb50
Nicolás Londoño <nicolas.londono@veevart.com>
2023-05-21 23:10:17 -0500

    Add: login endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,10 +32,38 @@ const createUser = async (req, res = express.request) => {
     }
 };
 
-const loginUser = (req, res = express.request) => {
-    res.json({
-        ok: true,
-    });
+const loginUser = async (req, res = express.request) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El usuario no existe"
+            });
+        }
+
+        const passwordValid = bcrypt.compareSync(password, user.password);
+        if (!passwordValid) {
+            return res.status(400).json({
+                ok: false,
+                msg: "La contraseña no es válida"
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            error
+        });
+    }
 };
 
 const revalidateToken = (req, res = express.request) => {
